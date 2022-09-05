7cd404c2d9925605c0fdfb19837826223589ac76
ryankert01 <ryan@ryankert.cc>
2022-09-05 23:33:05 +0800

    set base property in vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -5,24 +5,23 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
     path: "/about/:slug",
     name: "About",
-    component: () =>
-      import("../views/About.vue")
+    component: () => import("../views/About.vue"),
   },
   {
     path: "/:catchAll(.*)",
     name: "404",
-    component: () => import("../views/NotFound.vue")
-  }
+    component: () => import("../views/NotFound.vue"),
+  },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+  history: createWebHistory("/pokemon-finder"),
+  routes,
 });
 
 export default router;
